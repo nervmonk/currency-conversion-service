a545d8b226b585fffe4501b99a3fcaceb2e64ec7
dwikyryan <dwikyryan@bantusaku.id>
2024-07-25 16:01:00 +0700

    add feign client

--- src/main/java/com/dwikyryan/microservices/currency_conversion_service/CurrencyConversionController.java
+++ src/main/java/com/dwikyryan/microservices/currency_conversion_service/CurrencyConversionController.java
@@ -2,15 +2,24 @@ package com.dwikyryan.microservices.currency_conversion_service;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyConversionController {
-    
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    private CurrencyExchangeProxy proxy;
+
+    @Autowired
+    public CurrencyConversionController(CurrencyExchangeProxy proxy){
+        this.proxy = proxy;
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment() + " feign");
     }
 }
